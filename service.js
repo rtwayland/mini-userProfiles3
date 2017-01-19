543109bbddbd2e7837b4fc4a5083fa82a7538b4e
raleigh wayland <rtwayland@gmail.com>
2017-01-19 11:55:30 -0700

    complete

--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    this.getUsers = function() {
+        var deferred = $q.defer();
+        return $http({
+            method: 'GET',
+            url: 'https://reqres.in/api/users?page=1'
+        }).then(function(response) {
+            var parsedResponse = response.data.data;
+            for (var i = 0; i < parsedResponse.length; i++) {
+                parsedResponse[i].first_name = 'Ralf'
+            }
+            deferred.resolve(parsedResponse);
+        })
+
+        return deferred.promise;
+    }
 
 });
